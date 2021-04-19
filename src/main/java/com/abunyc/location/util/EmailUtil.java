75d3d2cb00cebaf19923024bdc1ce139990066c1
Abu <adamuabu@ymail.com>
2021-04-19 17:03:46 +0100

    Email utility config

---
+++ src/main/java/com/abunyc/location/util/EmailUtil.java
@@ -0,0 +1,13 @@
+package com.abunyc.location.util;
+
+/**
+ * @author Abu
+ */
+
+public interface EmailUtil {
+	
+	void sendEmail(String toAddress, String subject, String body);
+
+}
+
+
---
+++ src/main/java/com/abunyc/location/util/EmailUtilImpl.java
@@ -0,0 +1,39 @@
+package com.abunyc.location.util;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+
+/**
+ * @author Abu
+ */
+
+public class EmailUtilImpl implements EmailUtil {
+	
+	@Autowired
+	private JavaMailSender sender;
+	
+	@Override
+	public void sendEmail(String toAddress, String subject, String body) {
+		
+		MimeMessage message = sender.createMimeMessage();
+		MimeMessageHelper helper = new MimeMessageHelper(message);
+		
+		try {
+			helper.setTo(toAddress);
+			helper.setSubject(subject);
+			helper.setText(body);
+		} catch (MessagingException e) {
+			e.printStackTrace();
+		}
+		
+		sender.send(message);
+		
+	}
+
+}
+
+
